baf197f20bad3114ba10dc0e4dfa3f52a308a500
vikkyB2 <vigneshs21225@gmail.com>
2020-07-06 09:58:54 +0530

    add changes for retailer and items table structure

--- apps/data/items.py
+++ apps/data/items.py
@@ -5,16 +5,27 @@ import logging
 
 
 def fetchItems():
-    logging.debug("fetchh items")
     scope =  ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name('apps/data/credentials.json',scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json',scope)
     client = gspread.authorize(creds)
-    logging.debug("time")
     sheet = client.open('salesforce_items').sheet1
     #row=[sheet.row_count-1,"bulb"]
     logging.debug(sheet.row_count)
     #sheet.insert_row(row,sheet.row_count)
     legislators = sheet.get_all_records()
-
-    #df = pd.DataFrame(legislators)
-    return legislators
+    df = pd.DataFrame(legislators)
+    columns = list(df)
+    values = df.values.tolist()
+     resp = {
+        "values":values,
+        "columns":columns,
+        "name": "TB_ITEMS",
+        "pk": ["id"],
+        "types": [
+            'VARCHAR(45)',
+            'VARCHAR(45)',
+            'VARCHAR(45)',
+            'VARCHAR(45)',
+        ]
+    }
+    return resp
--- apps/data/rtlr.py
+++ apps/data/rtlr.py
@@ -16,6 +16,20 @@ def fetchRtlr():
     logging.debug(sheet.row_count)
     #sheet.insert_row(row,sheet.row_count)
     legislators = sheet.get_all_records()
-
+    df = pd.DataFrame(legislators)
+    columns = list(df)
+    values = df.values.tolist()
+    resp = {
+        "values":values,
+        "columns":columns,
+        "name": "TB_CUST",
+        "pk": ["id"],
+        "types": [
+            'VARCHAR(45)',
+            'VARCHAR(45)',
+            'VARCHAR(45)',
+            'VARCHAR(45)',
+        ]
+    }
     #df = pd.DataFrame(legislators)
-    return legislators
+    return resp
